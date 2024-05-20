0dd32e3514d3ce7e143620dfd074660305f5c9ab
Wellington Cavalcante <wellarts@gmail.com>
2024-05-20 13:46:19 -0300

    agenda

--- app/Filament/Resources/AgendamentoResource.php
+++ app/Filament/Resources/AgendamentoResource.php
@@ -22,6 +22,7 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class AgendamentoResource extends Resource
 {
+    
     protected static ?string $model = Agendamento::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
