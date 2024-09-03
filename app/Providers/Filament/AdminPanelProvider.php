bd939a8a40bd681d53bfbb7f5cbf6ac2be36b344
Wellington Cavalcante <wellarts@gmail.com>
2024-09-03 10:16:06 -0300

    export

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,6 +33,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->databaseNotifications()
             ->favicon(asset('img/logo.png'))
             ->brandLogo(asset('img/logo.png'))
             ->brandLogoHeight('3rem')
